a3fde3640887cd81e16ee4460054d8533527f6bf
mohammedyh <mohammedyousufhamid@gmail.com>
2025-06-11 16:42:08 +0100

    feat: setup a content collection for blog posts

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [mdx()],
 });
--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import eslintPluginAstro from "eslint-plugin-astro";
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
   ...eslintPluginAstro.configs.recommended,
   ...eslintPluginAstro.configs["jsx-a11y-recommended"],
